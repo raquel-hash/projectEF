987abd9878758d203bc10fa4ef361fed0b9f6864
Raquel <quelcaraquel1@gmail.com>
2024-05-24 10:19:09 -0400

    Creando modelo de categoría con Fluent API

--- Models/Category.cs
+++ Models/Category.cs
@@ -4,10 +4,10 @@ namespace projectEF.Models;
 
 public class Category
 {
-    [Key]
+    // [Key]
     public Guid Id { get; set; }
-    [Required]
-    [MaxLength(150)]
+    // [Required]
+    // [MaxLength(150)]
     public string Name { get; set; }
     public string Description { get; set; }
     public virtual ICollection<Task> Tasks { get; set; }
--- TaskContext.cs
+++ TaskContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using projectEF.Models;
 
 namespace projectEF;
@@ -11,4 +12,15 @@ public class TaskContext : DbContext
     public DbSet<Models.Task> tasks { get; set; }
 
     public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(category =>
+        {
+            category.ToTable("Category");
+            category.HasKey(p => p.Id);
+            category.Property(p => p.Name).IsRequired().HasMaxLength(150);
+            category.Property(p => p.Description);
+        });
+    }
 }
